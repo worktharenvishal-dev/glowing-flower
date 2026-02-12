760142dff456b9d1b030810cd0fa3590f973fa3f
worktharenvishal-dev <work.tharenvishal@gmail.com>
2026-02-12 20:57:48 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,22 @@
+
+onload = () => {
+  const c = setTimeout(() => {
+    document.body.classList.remove("not-loaded");
+
+    const titles = ('I LOVE YOU').split('')
+    const titleElement = document.getElementById('title');
+    let index = 0;
+
+    function appendTitle() {
+      if (index < titles.length) {
+        titleElement.innerHTML += titles[index];
+        index++;
+        setTimeout(appendTitle, 300); 
+      }
+    }
+
+    appendTitle();
+
+    clearTimeout(c);
+  }, 1000);
+};
