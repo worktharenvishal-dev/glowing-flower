70211af8b17e5f2fb42874a56c6d75204e47a698
worktharenvishal-dev <work.tharenvishal@gmail.com>
2026-02-12 21:06:30 +0800

    Change title text from 'MY ANGEL' to 'ANGELINA'

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ onload = () => {
   const c = setTimeout(() => {
     document.body.classList.remove("not-loaded");
 
-    const titles = ('I LOVE YOU, MY ANGEL').split('')
+    const titles = ('I LOVE YOU, ANGELINA').split('')
     const titleElement = document.getElementById('title');
     let index = 0;
 
@@ -21,3 +21,4 @@ onload = () => {
   }, 1000);
 
 };
+
