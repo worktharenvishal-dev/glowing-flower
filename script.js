a584f560a29071084d110bcba69b37e9bc04f085
worktharenvishal-dev <work.tharenvishal@gmail.com>
2026-02-12 20:58:40 +0800

    Change title text to 'I LOVE YOU, MY ANGEL'

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ onload = () => {
   const c = setTimeout(() => {
     document.body.classList.remove("not-loaded");
 
-    const titles = ('I LOVE YOU').split('')
+    const titles = ('I LOVE YOU, MY ANGEL').split('')
     const titleElement = document.getElementById('title');
     let index = 0;
 
@@ -19,4 +19,5 @@ onload = () => {
 
     clearTimeout(c);
   }, 1000);
+
 };
